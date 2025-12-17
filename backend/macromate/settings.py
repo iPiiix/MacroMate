f93fdff610f9868d51ff6241f639ac74f72f263b
iPix <santiperezguerrero2005@gmail.com>
2025-12-17 09:42:05 +0100

    Actualizar ALLOWED_HOSTS para permitir todas las conexiones

--- backend/macromate/settings.py
+++ backend/macromate/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-in-production')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,0.0.0.0').split(',')
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
